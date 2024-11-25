68519a3cd9632bb5c809452d2853badad3eab9ce
Gian-vie <gianvieceli@gmail.com>
2024-11-25 19:06:53 -0300

    inicio da prova p2
    declaração inicial e criação dos arquivos da prova p2 de desenvolvimento de interface web

---
+++ desenvovimento de interface web/Prova_P2/src/listaDeProdutos.jsx
@@ -0,0 +1,26 @@
+// Listar Produtos (GET)
+//  i. Exibir uma lista de produtos obtida da Fake Store API (/products).
+//  ii. Cada item deve exibir nome, imagem e preço do produto.
+//  iii. Permitir que o usuário clique no produto para visualizar mais detalhes.
+// b. Página de Detalhes do Produto (GET:id):
+//  i. Exibir detalhes completos do produto (imagem, título, descrição, categoria e
+// preço) obtidos de /products/{id}.
+// c. Adicionar Produtos (POST):
+//  i. Criar um formulário para adicionar um novo produto à API utilizando o endpoint
+// POST /products.
+//  ii. Campos obrigatórios: nome, preço, descrição, imagem (URL) e categoria.
+// d. Editar Produtos (PUT):
+//  i. Criar uma página de edição que permita atualizar os dados de um produto
+// existente utilizando o endpoint PUT /products/{id}.
+//  ii. Preencher os campos do formulário com os dados atuais do produto, permitindo
+// alterações.
+// e. Excluir Produtos (DELETE):
+//  i. Adicionar um botão de "Excluir" em cada produto listado, permitindo a remoção
+// do produto da API utilizando o endpoint DELETE /products/{id}.
+// f. Navegação:
+//  i. Configurar rotas para as páginas: Página Inicial, Detalhes do Produto, Carrinho,
+// Checkout, Adicionar Produto e Editar Produto.
+// g. Estilização:
+//  i. Utilizar estilização CSS (pode ser com CSS Modules, styled-components ou
+// outra abordagem).
+//  ii. Garantir que a interface seja visualmente agradável.
