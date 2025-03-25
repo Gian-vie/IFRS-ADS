1be78b0845eb3a4a1f6393bd5e88390e879fd361
Gian-vie <gianvieceli@gmail.com>
2025-03-24 21:26:33 -0300

    OO: private e public no banco

--- Orientação a objetos/OO/softweres.ts
+++ Orientação a objetos/OO/softweres.ts
@@ -1,11 +1,31 @@
 export class Software {
-  nome: string;
-  qtdMemoriaRAM: number;
-  qntHD: number;
+  private _nome: string;
+  public get nome(): string {
+    return this._nome;
+  }
+  public set nome(value: string) {
+    this._nome = value;
+  }
 
-  constructor(nome: string, qtdMemoriaRAM: number, qntHD: number) {
-    this.nome = nome;
-    this.qtdMemoriaRAM = qtdMemoriaRAM;
-    this.qntHD = qntHD;
+  private _qntRAM: number;
+  public get qtdRAM(): number {
+    return this._qntRAM;
+  }
+  public set qtdRAM(value: number) {
+    this._qntRAM = value;
+  }
+
+  private _qntHD: number;
+  public get qntHD(): number {
+    return this._qntHD;
+  }
+  public set qntHD(value: number) {
+    this._qntHD = value;
+  }
+
+  constructor(nome?: string, qtdRAM?: number, qntHD?: number) {
+    nome != null ? (this._nome = nome) : null;
+    qtdRAM != null ? (this._qntRAM = qtdRAM) : null;
+    qntHD != null ? (this._qntHD = qntHD) : null;
   }
 }
