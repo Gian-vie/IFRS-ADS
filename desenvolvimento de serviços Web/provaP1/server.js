0bccc30a267607aaeaae795b5bc2ae17f908f001
Gian Vieceli <gianvieceli@gmail.com>
2025-04-28 02:02:23 -0300

    fix: correção no server.js

--- desenvolvimento de serviços Web/provaP1/server.js
+++ desenvolvimento de serviços Web/provaP1/server.js
@@ -1,17 +1,17 @@
 const express = require("express");
-const connectDB = require("./config/db");
 
 const jogadoresRoutes = require("./routes/jogadoresRoutes");
 const jogoRoutes = require("./routes/jogoRoutes");
+const pontuacaoRoutes = require("./routes/pontuacaoRoutes")
 
 const app = express();
 const PORT = 3001;
 
-connectDB();
 app.use(express.json());
 
 app.use("/jogadores", jogadoresRoutes);
 app.use("/jogos", jogoRoutes);
+app.use("/pontuacao", pontuacaoRoutes)
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
