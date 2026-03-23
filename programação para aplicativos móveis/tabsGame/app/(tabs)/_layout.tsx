629c927cfd46d8ac8451793e5b341062372220ca
Gian Carlos Vieceli <gianvieceli@gmail.com>
2026-03-23 20:45:09 -0300

    n finalizado projeto games

--- programação para aplicativos móveis/tabsGame/app/(tabs)/_layout.tsx
+++ programação para aplicativos móveis/tabsGame/app/(tabs)/_layout.tsx
@@ -1,33 +1,34 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+export default function tabsLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
+        tabBarActiveTintColor: "red",
+        tabBarInactiveTintColor: "gray",
+        tabBarStyle: {
+          backgroundColor: "black",
+        },
+      }}
+    >
       <Tabs.Screen
-        name="index"
+        name="game"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: "Jogo",
+          tabBarIcon: ({ size, color }) => (
+            <Ionicons name="game-controller" size={size} color={color} />
+          ),
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="settings"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: "Configurações",
+          tabBarIcon: ({ size, color }) => (
+            <Ionicons name="settings" size={size} color={color} />
+          ),
         }}
       />
     </Tabs>
