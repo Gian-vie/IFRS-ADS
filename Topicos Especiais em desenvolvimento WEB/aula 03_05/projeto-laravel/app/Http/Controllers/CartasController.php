d18b4c77ccdc7e4f1716f89e4e4367bc2754de92
Gian Carlos Vieceli <gianvieceli@gmail.com>
2026-04-02 22:01:54 -0300

    subindo no eio da aula

--- Topicos Especiais em desenvolvimento WEB/aula 03_05/projeto-laravel/app/Http/Controllers/CartasController.php
+++ Topicos Especiais em desenvolvimento WEB/aula 03_05/projeto-laravel/app/Http/Controllers/CartasController.php
@@ -2,18 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Carta;
 use Illuminate\Http\Request;
 
 class CartasController extends Controller
 {
     public function index () {
-        return view('cartas/index');
+        $cartas = Carta::all();
+
+        return view('cartas.index', [
+            'cartas' => $cartas
+        ]);
     }
 
     public function inserir(Request $request) {
         if ($request->isMethod('POST')){
             // dd($request->all());
-            dd($request->only('nome', 'tipo', 'foto'));
+            $dados = $request->only('nome', 'tipo'); 
+            // dd($dados);
+            Carta::create($dados);
+            
+            return redirect()->route('cartas.index')->with('success', 'Carta inserida com sucesso!');
+        
         }
         return view('cartas.inserir');
 
